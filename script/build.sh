6b354121bd9315587a96e133681c69a14545e972
liudonghai <liudhzhyym@163.com>
2017-10-13 12:09:03 +0800

    update

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 version=`grep '^version=' ./install-cow.sh | sed -s 's/version=//'`
@@ -33,26 +34,26 @@ build() {
     echo "building $name"
     echo $cgo $goos $goarch $goarm go build
     eval $cgo $goos $goarch $goarm go build || exit 1
-    if [[ $1 == "windows" ]]; then
-        mv cow.exe script
-        pushd script
-        sed -e 's/$/\r/' ../doc/sample-config/rc > rc.txt
-        zip $name.zip cow.exe cow-taskbar.exe cow-hide.exe rc.txt
-        rm -f cow.exe rc.txt
-        mv $name.zip ../bin/
-        popd
-    else
-        mv cow bin/$name
-        gzip -f bin/$name
-    fi
+    # if [[ $1 == "windows" ]]; then
+    #     mv cow.exe script
+    #     pushd script
+    #     sed -e 's/$/\r/' ../doc/sample-config/rc > rc.txt
+    #     zip $name.zip cow.exe cow-taskbar.exe cow-hide.exe rc.txt
+    #     rm -f cow.exe rc.txt
+    #     mv $name.zip ../bin/
+    #     popd
+    # else
+    #     mv cow bin/$name
+    #     gzip -f bin/$name
+    # fi
 }
 
 build darwin amd64 mac64
 #build darwin 386 mac32
-build linux amd64 linux64
-build linux 386 linux32
-build linux arm linux-armv5tel
-build linux arm linux-armv6l
-build linux arm linux-armv7l
-build windows amd64 win64
-build windows 386 win32
+# build linux amd64 linux64
+# build linux 386 linux32
+# build linux arm linux-armv5tel
+# build linux arm linux-armv6l
+# build linux arm linux-armv7l
+# build windows amd64 win64
+# build windows 386 win32
